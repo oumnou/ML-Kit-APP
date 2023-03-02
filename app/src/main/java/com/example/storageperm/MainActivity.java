65441b0b06a3b3bdc9c43583fde6798333feb57b
Oumaima Nouni <oumaimanouni08@gmail.com>
2023-03-02 13:21:38 +0100

    Add Image classification using Google ML Kit

--- app/src/main/java/com/example/storageperm/MainActivity.java
+++ app/src/main/java/com/example/storageperm/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.storageperm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.storageperm.helpers.ImageHelperActivity;
 
--- app/src/main/java/com/example/storageperm/helpers/ImageHelperActivity.java
+++ app/src/main/java/com/example/storageperm/helpers/ImageHelperActivity.java
@@ -4,17 +4,14 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Build;
-import android.os.StrictMode;
-import android.os.strictmode.Violation;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,14 +19,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.storageperm.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class ImageHelperActivity extends AppCompatActivity {
     private int REQUEST_PICK_IMAGE = 1000;
     private ImageView inputImageView;
     private TextView outputTextView;
+
+    private ImageLabeler imageLabeler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,11 @@ public class ImageHelperActivity extends AppCompatActivity {
 
         inputImageView = findViewById(R.id.imageViewInput);
         outputTextView = findViewById(R.id.textViewOutput);
+
+        imageLabeler = ImageLabeling.getClient(new ImageLabelerOptions
+                .Builder()
+                .setConfidenceThreshold(0.7f)
+                .build());
 
         if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M) {
             if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -69,8 +81,8 @@ public class ImageHelperActivity extends AppCompatActivity {
           //  if(resultCode == REQUEST_PICK_IMAGE){
                 Uri uri = data.getData();
                 Bitmap bitmap = loadFromUri(uri);
-
                 inputImageView.setImageBitmap(bitmap);
+                runClassification(bitmap);
 
 
 
@@ -95,4 +107,31 @@ public class ImageHelperActivity extends AppCompatActivity {
 
         return bitmap;
     }
+
+    private void runClassification(Bitmap bitmap){
+        InputImage inputImage = InputImage.fromBitmap(bitmap,0);
+        imageLabeler.process(inputImage).addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+            @Override
+            public void onSuccess(List<ImageLabel> imageLabels) {
+                if (imageLabels.size() > 0) {
+                    StringBuilder builder = new StringBuilder();
+                    for (ImageLabel label : imageLabels){
+                        builder.append(label.getText())
+                                .append(" : ")
+                                .append(label.getConfidence())
+                                .append("\n");
+                    }
+                    outputTextView.setText(builder.toString());
+                } else {
+                    outputTextView.setText("Could not classify");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@androidx.annotation.NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
 }
